9af403ae4a97d2b0416385c3468de820c8633908
tmw <>
2008-01-31 06:35:24 +0000

    Updated database model

--- Dialogix_J2EE/DialogixEntities/src/java/org/dialogix/entities/InstrumentHash.java
+++ Dialogix_J2EE/DialogixEntities/src/java/org/dialogix/entities/InstrumentHash.java
@@ -28,7 +28,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "instrument_hashes")
 public class InstrumentHash implements Serializable {
-    @TableGenerator(name="instrument_hash_gen", pkColumnValue="instrument_hash_gen", table="sequence", pkColumnName="seq_name", valueColumnName="seq_count", allocationSize=1)
+    @TableGenerator(name="instrument_hash_gen", pkColumnValue="instrument_hash", table="sequence", pkColumnName="seq_name", valueColumnName="seq_count", allocationSize=1)
     @Id
     @GeneratedValue(strategy=GenerationType.TABLE, generator="instrument_hash_gen")
     @Column(name = "instrument_hash_id", nullable = false)
